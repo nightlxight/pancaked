99d87d94fe6295226cff3668d5792d7deffacce8
nightlxight <itsbeadj@gmail.com>
2021-09-18 15:09:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const foods = yaml.load(fs.readFileSync('./data/foods.yml'));
 
 function cakeFunction() {
     let cakes = [];
-    let cake = cakes[Math.floor(Math.random() * cakes.length)]
+    let cake = cakes[Math.floor(Math.random() * cakes.length)];
 }
